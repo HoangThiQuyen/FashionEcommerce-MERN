14111342c2e101631c5162d751a7990702105df6
quyen <hoangquyen99@gmail.com>
2022-01-21 01:09:30 +0700

    updated 21/1

--- server/routes/Stripe.js
+++ server/routes/Stripe.js
@@ -2,6 +2,6 @@ const router = require("express");
 const stripeRouter = router.Router();
 const { payment } = require("../controllers/Stripe");
 
-router.post("/payment", payment);
+stripeRouter.post("/payment", payment);
 
 module.exports = stripeRouter;
